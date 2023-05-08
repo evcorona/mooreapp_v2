994dea350fc2cd809dc9ebaa478cf906c3417d61
evcorona <eldav.corona@gmail.com>
2023-05-07 19:44:40 -0600

    🔧 Update config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        login: "url('./src/images/bg-login.png')",
+        main: "url('./src/images/bg-main.png')",
+        logo: "url('./src/images/moore-logo.png')",
+      },
+      colors: {
+        moore: {
+          DEFAULT: '#29aae1',
+          dark: '#007baf',
+          light: '#6fdcff',
+        },
+        alert: {
+          warning: '#eab308',
+          info: '#3b82f6',
+          error: '#ef4444',
+          success: '#22c55e',
+        },
+      },
+    },
   },
   plugins: [],
 }
