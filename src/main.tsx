59acf0775281c28f6de2a463fe26c6ee9d9eb412
evcorona <eldav.corona@gmail.com>
2023-05-09 01:09:17 -0600

    ✨ Add the navigation router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,31 @@
 import './styles/global.scss'
 
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 import App from './App.tsx'
-import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <ToastContainer
+        position="top-center"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        limit={3}
+        theme="colored"
+      />
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
 )
