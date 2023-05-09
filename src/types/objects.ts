a31d337b89c2aee92a094e9a3f8828b507be89de
evcorona <eldav.corona@gmail.com>
2023-05-09 10:39:20 -0600

    🏷️ Add global types

--- src/types/objects.ts
+++ src/types/objects.ts
@@ -9,3 +9,54 @@ export type NavBarProps = {
     to: string
   }[]
 }
+
+export type ClientsData = {
+  _id: string
+  clientName: string
+  group: string
+  codeClient: string
+  RFC: string
+  address: string
+  isActive: boolean
+}
+
+export type ProjectsData = {
+  _id: string
+  clientName: string
+  projectName: string
+  codeProject: string
+  isActive: boolean
+}
+
+export type CollaboratorsData = {
+  _id: string
+  email: string
+  rol: string
+  name: string
+  lastName: string
+  positionJob: string
+  fee: number
+  partner: string
+  area: string
+  employeeNumber: string | number
+  employmentDate: Date
+  isActive: boolean
+}
+
+export type ActivitiesData = {
+  _id: string
+  activityTYpe: string
+  timeAmmount: number
+  client: string
+  concept: string
+  activityDate: Date
+  createdBy: string
+  fee: number
+  isActive?: boolean
+}
+
+export type CollectionsDataType =
+  | ClientsData
+  | ProjectsData
+  | CollaboratorsData
+  | ActivitiesData
