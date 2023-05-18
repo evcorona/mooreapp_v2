ce3b802b6fd532cf1ccb99ae8bd6b0314f7177ad
evcorona <eldav.corona@gmail.com>
2023-05-18 00:42:36 -0600

    ⚰️ Remove dead code, wip and files

--- src/pages/Administrator/Details.tsx
+++ src/pages/Administrator/Details.tsx
@@ -1,19 +1,19 @@
 import Title from '~/components/Title'
-import ToolsBar from '~/components/Template/AdminPageTemplate/ToolsBar'
-import { useState } from 'react'
+// import ToolsBar from '~/components/Template/AdminPageTemplate/ToolsBar'
+// import { useState } from 'react'
 
 export default function Details() {
-  const [searchInput, setSearchInput] = useState('')
+  // const [searchInput, setSearchInput] = useState('')
 
   return (
-    <div className="container mx-auto space-y-4 pt-20 px-4">
+    <div className="container mx-auto space-y-4 px-4 pt-20">
       <Title title={'detalles del cliente'} />
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="card-title">Card title!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn-primary btn">Buy Now</button>
           </div>
         </div>
       </div>
@@ -22,11 +22,11 @@ export default function Details() {
           <h2 className="card-title">Card title!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn-primary btn">Buy Now</button>
           </div>
         </div>
       </div>
-      <ToolsBar setSearchInput={setSearchInput} data={[]} placeholder={''} />
+      {/* <ToolsBar setSearchInput={setSearchInput} data={[]} placeholder={''} /> */}
     </div>
   )
 }
